02ad3f7a35dc05f61b1e8eff50e2334427d64997
chenkaihui <chenkaihui@wps.cn>
2018-01-02 16:49:09 +0800

    添加用户分组管理

--- group/group.go
+++ group/group.go
@@ -13,35 +13,47 @@ const (
 	groupCreateURL = "/groups/create"
 	groupGetURL    = "/groups/get"
 	groupUpdateURL = "/groups/members/update"
+	groupUserURL   = "/groups/getid"
 )
 
 type Group struct {
 	*context.Context
 }
 
-type ResGroup struct {
+type ResGroupList struct {
 	util.CommonError
-	Groups []struct {
-		Name    string `json:"name"`
-		GroupId int64  `json:"id"`
-		Count   int64  `json:"count"`
-	} `json:"groups"`
+	Groups []SelfGroup `json:"groups"`
 }
 
-type RecCreateGroup struct {
+type ResCreateGroup struct {
 	util.CommonError
 	Group struct {
 		Id int64 `json:"id"`
 	}
 }
 
-type reqGroup struct {
-	Name string `json:name`
+type ResUserGroup struct {
+	util.CommonError
+	GroupId int64 `json:"groupid"`
+}
+
+type SelfGroup struct {
+	Name    string `json:"name,omitempty"`
+	GroupId int64  `json:"id,omitempty"`
+	Count   int64  `json:"count,omitempty"`
+}
+
+type reqCreateGroup struct {
+	Group SelfGroup `json:"group"`
 }
 
 type reqUpdateGroup struct {
-	Openid    string `json:openid`
-	ToGroupid string `json:to_groupid`
+	Openid    string `json:"openid"`
+	ToGroupid string `json:"to_groupid"`
+}
+
+type reqUserGroup struct {
+	Openid string `json:"openid"`
 }
 
 //NewGroup 实例
@@ -52,7 +64,7 @@ func NewGroup(context *context.Context) *Group {
 }
 
 //获取分组列表
-func (group *Group) GetGroup() (resGroup ResGroup, err error) {
+func (group *Group) GetGroup() (resGroup ResGroupList, err error) {
 	var accessToken string
 	accessToken, err = group.GetAccessToken()
 	if err != nil {
@@ -110,8 +122,40 @@ func (group *Group) AddGroup(groupName string) (groupId int64, err error) {
 		return
 	}
 	uri := fmt.Sprintf("%s%s?access_token=%s", baseApi, groupCreateURL, accessToken)
-	reqGroup := &reqGroup{
+	selfGroup := SelfGroup{
 		Name: groupName,
+	}
+	reqCreateGroup := &reqCreateGroup{
+		Group: selfGroup,
+	}
+
+	var response []byte
+	response, err = util.PostJSON(uri, reqCreateGroup)
+	if err != nil {
+		return
+	}
+	var resCreateGroup ResCreateGroup
+	err = json.Unmarshal(response, &resCreateGroup)
+	if err != nil {
+		return
+	}
+	if resCreateGroup.ErrCode != 0 {
+		err = fmt.Errorf("AddGroup Error , errcode=%d , errmsg=%s", resCreateGroup.ErrCode, resCreateGroup.ErrMsg)
+		return
+	}
+	groupId = resCreateGroup.Group.Id
+	return
+}
+
+//获取用户所在分组
+func (group *Group) GetUserGroup(openid string) (groupId int64, err error) {
+	accessToken, err := group.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s%s?access_token=%s", baseApi, groupUserURL, accessToken)
+	reqGroup := &reqUserGroup{
+		Openid: openid,
 	}
 
 	var response []byte
@@ -119,15 +163,15 @@ func (group *Group) AddGroup(groupName string) (groupId int64, err error) {
 	if err != nil {
 		return
 	}
-	var recCreateGroup RecCreateGroup
-	err = json.Unmarshal(response, &recCreateGroup)
+	var resUserGroup ResUserGroup
+	err = json.Unmarshal(response, &resUserGroup)
 	if err != nil {
 		return
 	}
-	if recCreateGroup.ErrCode != 0 {
-		err = fmt.Errorf("AddGroup Error , errcode=%d , errmsg=%s", recCreateGroup.ErrCode, recCreateGroup.ErrMsg)
+	if resUserGroup.ErrCode != 0 {
+		err = fmt.Errorf("AddGroup Error , errcode=%d , errmsg=%s", resUserGroup.ErrCode, resUserGroup.ErrMsg)
 		return
 	}
-	groupId = recCreateGroup.Group.Id
+	groupId = resUserGroup.GroupId
 	return
 }
