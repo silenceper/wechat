424abf819b32088b576fd1149775710fb6533a6c
解闯 <chuang.xie@enai.im>
2018-04-26 14:28:50 +0800

    add gopkg.in redisv3

--- cache/redisv3.go
+++ cache/redisv3.go
@@ -3,52 +3,72 @@ package cache
 import (
 	"time"
 
-	"github.com/kataras/golog"
 	redis "gopkg.in/redis.v3"
 )
 
-var rdsClient *redis.Client
+/*
+	使用方式:
+		1. 通过NewRdsV3Client建立连接后使用
+		2. 通过RdsV3ClientInit初始化获得对象(与外部服务共用连接)
+*/
 
-// Init 初始化 redis client
-func CacheInit(client *redis.Client) {
-	if rdsClient == nil {
-		rdsClient = client
-	}
+// RedisV3 client
+type RedisV3 struct {
+	rdsClient *redis.Client
 }
 
-// checkRedis check
-func checkRedis() {
-	if rdsClient == nil {
-		golog.Error("wx server cache: redis client is nil")
+// RedisConfig redis连接配置
+type RedisConfig struct {
+	Address     string `yml:"address" json:"address"`
+	Password    string `yml:"password" json:"password"`
+	Timeout     int    `yml:"timeout" json:"timeout"`
+	MaxIdle     int    `yml:"max_idle" json:"max_idle"`
+	IdleTimeout int    `yml:"idle_timeout" json:"idle_timeout"`
+}
+
+// 初始化redisclient
+func NewRdsV3Client(c *RedisConfig) *RedisV3 {
+	client := redis.NewClient(&redis.Options{
+		Addr:         c.Address,
+		Password:     c.Password,
+		PoolSize:     c.MaxIdle,
+		IdleTimeout:  time.Duration(c.IdleTimeout) * time.Second,
+		DialTimeout:  time.Duration(c.Timeout) * time.Second,
+		ReadTimeout:  time.Duration(c.Timeout) * time.Second,
+		WriteTimeout: time.Duration(c.Timeout) * time.Second,
+	})
+	return &RedisV3{client}
+}
+
+// RdsV3ClientInit 外部服务初始化(直接传入已初始化的redis client)
+func RdsV3ClientInit(client *redis.Client) *RedisV3 {
+	return &RedisV3{
+		rdsClient: client,
 	}
 }
 
 // Get
-func Get(key string) string {
-	checkRedis()
-	return rdsClient.Get(key).String()
+func (r *RedisV3) Get(key string) interface{} {
+	return r.rdsClient.Get(key).Val()
 }
 
 // Set
-func Set(key string, val interface{}, duration time.Duration) (err error) {
-	checkRedis()
-	if IsExist(key) {
-		err := Delete(key)
+func (r *RedisV3) Set(key string, val interface{}, duration time.Duration) (err error) {
+	if r.IsExist(key) {
+		err := r.Delete(key)
 		if err != nil {
 			return err
 		}
 	}
-	return rdsClient.Set(key, val, duration).Err()
+	return r.rdsClient.Set(key, val, duration).Err()
 }
 
 // IsExist false->non exist
-func IsExist(key string) bool {
-	checkRedis()
-	return rdsClient.Exists(key).Val()
+func (r *RedisV3) IsExist(key string) bool {
+	return r.rdsClient.Exists(key).Val()
 }
 
 // Delete
-func Delete(key string) error {
-	checkRedis()
-	return rdsClient.Del(key).Err()
+func (r *RedisV3) Delete(key string) error {
+	return r.rdsClient.Del(key).Err()
 }
