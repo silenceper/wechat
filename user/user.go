780515b5ee8cad78b76d1d1f749a0f6b1f38430c
sankeyou <trueugly@qq.com>
2018-06-10 12:20:27 +0800

    增加获取用户列表接口

--- user/user.go
+++ user/user.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	userInfoURL = "https://api.weixin.qq.com/cgi-bin/user/info"
+	userListURL = "https://api.weixin.qq.com/cgi-bin/user/get"
 )
 
 //User 用户管理
@@ -44,6 +45,17 @@ type Info struct {
 	TagidList     []string `json:"tagid_list"`
 }
 
+//UserList 用户列表
+type UserList struct {
+	util.CommonError
+	Total int64 `json:"total"`
+	Count int64 `json:"count"`
+	Data  struct {
+		OpenID []string `json:"openid"`
+	} `json:"data"`
+	NextOpenID string `json:"next_openid"`
+}
+
 //GetUserInfo 获取用户基本信息
 func (user *User) GetUserInfo(openID string) (userInfo *Info, err error) {
 	var accessToken string
@@ -65,6 +77,31 @@ func (user *User) GetUserInfo(openID string) (userInfo *Info, err error) {
 	}
 	if userInfo.ErrCode != 0 {
 		err = fmt.Errorf("GetUserInfo Error , errcode=%d , errmsg=%s", userInfo.ErrCode, userInfo.ErrMsg)
+		return
+	}
+	return
+}
+
+//GetUserLists 获取用户列表
+func (user *User) GetUserLists(nextOpenID string) (userList *UserList, err error) {
+	var accessToken string
+	accessToken, err = user.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s?access_token=%s&next_openid=%s&lang=zh_CN", userListURL, accessToken, nextOpenID)
+	var response []byte
+	response, err = util.HTTPGet(uri)
+	if err != nil {
+		return
+	}
+	userList = new(UserList)
+	err = json.Unmarshal(response, userList)
+	if err != nil {
+		return
+	}
+	if userList.ErrCode != 0 {
+		err = fmt.Errorf("GetUserInfo Error , errcode=%d , errmsg=%s", userList.ErrCode, userList.ErrMsg)
 		return
 	}
 	return
