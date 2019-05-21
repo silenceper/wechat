bf11a393e3fd1d8a9ce1ccad9f4e141079dd76fd
zdpdpdp <zdpdp@sina.cn>
2019-05-21 12:47:31 +0800

    add: 自定义获取 access token 方法

--- context/context.go
+++ context/context.go
@@ -27,6 +27,9 @@ type Context struct {
 
 	//jsAPITicket 读写锁 同一个AppID一个
 	jsAPITicketLock *sync.RWMutex
+
+	//自定义获取 access token 的方法
+	accessTokenFunc CustomGetAccessToken
 }
 
 // Query returns the keyed url query value if it exists
