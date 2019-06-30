612cf78f95dcd2b1e50cbce42527c730552f95a1
xiaodong <jokimina@163.com>
2019-07-01 00:05:13 +0800

    add: weapp template support

--- template/template.go
+++ template/template.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	templateSendURL = "https://api.weixin.qq.com/cgi-bin/message/template/send"
+	templateSendURL      = "https://api.weixin.qq.com/cgi-bin/message/template/send"
+	templateWeappSendURL = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send"
 )
 
 //Template 模板消息
@@ -28,7 +29,9 @@ func NewTemplate(context *context.Context) *Template {
 type Message struct {
 	ToUser     string               `json:"touser"`          // 必须, 接受者OpenID
 	TemplateID string               `json:"template_id"`     // 必须, 模版ID
+	FormID     string               `json:"form_id"`         // 必须, 前端提交的表单ID
 	URL        string               `json:"url,omitempty"`   // 可选, 用户点击后跳转的URL, 该URL必须处于开发者在公众平台网站中设置的域中
+	Page       string               `json:"page"`            //所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar）
 	Color      string               `json:"color,omitempty"` // 可选, 整个消息的颜色, 可以不设置
 	Data       map[string]*DataItem `json:"data"`            // 必须, 模板数据
 
@@ -51,13 +54,18 @@ type resTemplateSend struct {
 }
 
 //Send 发送模板消息
-func (tpl *Template) Send(msg *Message) (msgID int64, err error) {
-	var accessToken string
+func (tpl *Template) Send(msg *Message, isWeapp bool) (msgID int64, err error) {
+	var sendUrl, accessToken string
 	accessToken, err = tpl.GetAccessToken()
 	if err != nil {
 		return
 	}
-	uri := fmt.Sprintf("%s?access_token=%s", templateSendURL, accessToken)
+	if isWeapp {
+		sendUrl = templateWeappSendURL
+	} else {
+		sendUrl = templateSendURL
+	}
+	uri := fmt.Sprintf("%s?access_token=%s", sendUrl, accessToken)
 	response, err := util.PostJSON(uri, msg)
 
 	var result resTemplateSend
