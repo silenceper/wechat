f6d07aa714048acd5e6371e69eecf816a412a66d
陈灿鑫 <chencanxin@forchange.tech>
2019-11-08 11:17:11 +0800

    fix: 修复 CDATA.MarshalXML 注释

--- message/message.go
+++ message/message.go
@@ -187,7 +187,7 @@ type CommonToken struct {
 // CDATA  使用该类型,在序列化为 xml 文本时文本会被解析器忽略
 type CDATA string
 
-//  MarshalXML 实现自己的序列化方法
+// MarshalXML 实现自己的序列化方法
 func (c CDATA) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeElement(struct {
 		string `xml:",cdata"`
