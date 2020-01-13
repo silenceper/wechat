0e14ca105f327e5bef91d0d5776c40949dd54559
kaiiak <aNxFi37X@outlook.com>
2020-01-13 22:44:34 +0800

    小程序解密手机号

--- miniprogram/decrypt.go
+++ miniprogram/decrypt.go
@@ -21,16 +21,19 @@ var (
 
 // UserInfo 用户信息
 type UserInfo struct {
-	OpenID    string `json:"openId"`
-	UnionID   string `json:"unionId"`
-	NickName  string `json:"nickName"`
-	Gender    int    `json:"gender"`
-	City      string `json:"city"`
-	Province  string `json:"province"`
-	Country   string `json:"country"`
-	AvatarURL string `json:"avatarUrl"`
-	Language  string `json:"language"`
-	Watermark struct {
+	OpenID          string `json:"openId"`
+	UnionID         string `json:"unionId"`
+	NickName        string `json:"nickName"`
+	Gender          int    `json:"gender"`
+	City            string `json:"city"`
+	Province        string `json:"province"`
+	Country         string `json:"country"`
+	AvatarURL       string `json:"avatarUrl"`
+	Language        string `json:"language"`
+	PhoneNumber     string `json:"phoneNumber"`
+	PurePhoneNumber string `json:"purePhoneNumber"`
+	CountryCode     string `json:"countryCode"`
+	Watermark       struct {
 		Timestamp int64  `json:"timestamp"`
 		AppID     string `json:"appid"`
 	} `json:"watermark"`
