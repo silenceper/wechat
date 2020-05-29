5e8e16444cc6ad26195a6ce7b5c81e599b5668a6
silenceper <silenceper@gmail.com>
2020-05-29 23:17:04 +0800

    群发消息接口 (#259)
    * 添加TODO：待完善接口
    
    * 【模板消息】将message.DataItem改为message.TemplateDataItem
    
    * 【群发消息】基本框架
    
    * 群发消息-基本方法
    
    * fix golint
    
    * fix:SendWxCard log

--- officialaccount/message/template.go
+++ officialaccount/message/template.go
@@ -24,13 +24,13 @@ func NewTemplate(context *context.Context) *Template {
 	return tpl
 }
 
-//Message 发送的模板消息内容
-type Message struct {
-	ToUser     string               `json:"touser"`          // 必须, 接受者OpenID
-	TemplateID string               `json:"template_id"`     // 必须, 模版ID
-	URL        string               `json:"url,omitempty"`   // 可选, 用户点击后跳转的URL, 该URL必须处于开发者在公众平台网站中设置的域中
-	Color      string               `json:"color,omitempty"` // 可选, 整个消息的颜色, 可以不设置
-	Data       map[string]*DataItem `json:"data"`            // 必须, 模板数据
+//TemplateMessage 发送的模板消息内容
+type TemplateMessage struct {
+	ToUser     string                       `json:"touser"`          // 必须, 接受者OpenID
+	TemplateID string                       `json:"template_id"`     // 必须, 模版ID
+	URL        string                       `json:"url,omitempty"`   // 可选, 用户点击后跳转的URL, 该URL必须处于开发者在公众平台网站中设置的域中
+	Color      string                       `json:"color,omitempty"` // 可选, 整个消息的颜色, 可以不设置
+	Data       map[string]*TemplateDataItem `json:"data"`            // 必须, 模板数据
 
 	MiniProgram struct {
 		AppID    string `json:"appid"`    //所需跳转到的小程序appid（该小程序appid必须与发模板消息的公众号是绑定关联关系）
@@ -38,8 +38,8 @@ type Message struct {
 	} `json:"miniprogram"` //可选,跳转至小程序地址
 }
 
-//DataItem 模版内某个 .DATA 的值
-type DataItem struct {
+//TemplateDataItem 模版内某个 .DATA 的值
+type TemplateDataItem struct {
 	Value string `json:"value"`
 	Color string `json:"color,omitempty"`
 }
@@ -51,7 +51,7 @@ type resTemplateSend struct {
 }
 
 //Send 发送模板消息
-func (tpl *Template) Send(msg *Message) (msgID int64, err error) {
+func (tpl *Template) Send(msg *TemplateMessage) (msgID int64, err error) {
 	var accessToken string
 	accessToken, err = tpl.GetAccessToken()
 	if err != nil {
