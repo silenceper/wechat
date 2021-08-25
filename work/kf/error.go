f637a8ed1c01d10d40c7786b40c447447adc1d5d
Afeyer <1500527791@qq.com>
2021-08-26 01:22:10 +0800

    feat:对外暴露SDKApiForbidden等错误
    可以通过调用升级服务相关接口然后根据该错误判断微信客服配置来源

--- work/kf/error.go
+++ work/kf/error.go
@@ -31,6 +31,10 @@ const (
 	SDKAccessTokenExpired Error = "AccessToken 已过期"
 	// SDKApiFreqOutOfLimit 错误码：45009
 	SDKApiFreqOutOfLimit Error = "接口请求次数超频"
+	// SDKApiForbidden 错误码：48002
+	SDKApiForbidden Error = "API 禁止调用"
+	// SDKOpenKFIDNotExist 错误码：95004
+	SDKOpenKFIDNotExist Error = "open_kfid 不存在"
 	// SDKWeWorkAlready 错误码：95011
 	SDKWeWorkAlready Error = "已在企业微信使用微信客服"
 )
@@ -63,6 +67,10 @@ func NewSDKErr(code int64, msgList ...string) Error {
 		return SDKDecryptMSGFailed
 	case 45009:
 		return SDKApiFreqOutOfLimit
+	case 48002:
+		return SDKApiForbidden
+	case 95004:
+		return SDKOpenKFIDNotExist
 	case 95011:
 		return SDKWeWorkAlready
 	default:
