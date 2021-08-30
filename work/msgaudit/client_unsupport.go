39dbfd1c13f5a7850598ef167817dc203b6bbaf1
silenceper <silenceper@gmail.com>
2021-08-30 18:00:08 +0800

    fix linux build failed when cgo disable (#453)

--- work/msgaudit/client_unsupport.go
+++ work/msgaudit/client_unsupport.go
@@ -1,4 +1,4 @@
-// +build !linux
+// +build !linux linux,!cgo
 
 //Package msgaudit for unsupport platform
 package msgaudit
@@ -15,5 +15,5 @@ type Client struct {
 
 // NewClient new
 func NewClient(cfg *config.Config) (*Client, error) {
-	return nil, fmt.Errorf("会话存档功能目前只支持Linux平台运行")
+	return nil, fmt.Errorf("会话存档功能目前只支持Linux平台运行，并且打开设置CGO_ENABLED=1")
 }
