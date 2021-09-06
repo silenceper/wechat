4b0d399da83d61993b62f4cc31e9fecf5ad1c382
daguang <lianghuanan1997@gmail.com>
2021-09-06 12:11:54 +0800

    小程序 shortLink

--- miniprogram/shortlink/shortlink.go
+++ miniprogram/shortlink/shortlink.go
@@ -2,6 +2,7 @@ package shortlink
 
 import (
 	"fmt"
+
 	"github.com/silenceper/wechat/v2/miniprogram/context"
 	"github.com/silenceper/wechat/v2/util"
 )
@@ -10,6 +11,7 @@ const (
 	generateShortLinkURL = "https://api.weixin.qq.com/wxa/genwxashortlink?access_token=%s"
 )
 
+// ShortLink 短链接
 type ShortLink struct {
 	*context.Context
 }
@@ -19,19 +21,20 @@ func NewShortLink(ctx *context.Context) *ShortLink {
 	return &ShortLink{ctx}
 }
 
+// ShortLinker 请求结构体
 type ShortLinker struct {
 
 	// pageUrl 通过 Short Link 进入的小程序页面路径，必须是已经发布的小程序存在的页面，可携带 query，最大1024个字符
-	PageUrl string `json:"page_url"`
+	PageURL string `json:"page_url"`
 
 	// pageTitle 页面标题，不能包含违法信息，超过20字符会用... 截断代替
 	PageTitle string `json:"page_title"`
 
 	// isPermanent 生成的 Short Link 类型，短期有效：false，永久有效：true
 	IsPermanent bool `json:"is_permanent,omitempty"`
-
 }
 
+// resShortLinker 返回结构体
 type resShortLinker struct {
 	// 通用错误
 	*util.CommonError
@@ -41,42 +44,42 @@ type resShortLinker struct {
 }
 
 // Generate 生成 shortLink
-func (shortLink *ShortLink) generate(shortLinkParams ShortLinker) (string,error) {
+func (shortLink *ShortLink) generate(shortLinkParams ShortLinker) (string, error) {
 	var accessToken string
 	accessToken, err := shortLink.GetAccessToken()
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	urlStr := fmt.Sprintf(generateShortLinkURL, accessToken)
 	response, err := util.PostJSON(urlStr, shortLinkParams)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	// 使用通用方法返回错误
 	var res resShortLinker
-	err = util.DecodeWithError(response,&res,"GenerateShortLink")
+	err = util.DecodeWithError(response, &res, "GenerateShortLink")
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
-	return res.Link,nil
+	return res.Link, nil
 }
 
 // GenerateShortLinkPermanent 生成永久shortLink
-func (shortLink *ShortLink) GenerateShortLinkPermanent(pageUrl,pageTitle string) (string,error) {
+func (shortLink *ShortLink) GenerateShortLinkPermanent(PageURL, pageTitle string) (string, error) {
 	return shortLink.generate(ShortLinker{
-		PageUrl:     pageUrl,
+		PageURL:     PageURL,
 		PageTitle:   pageTitle,
 		IsPermanent: true,
 	})
 }
 
 // GenerateShortLinkTemp 生成临时shortLink
-func (shortLink *ShortLink) GenerateShortLinkTemp(pageUrl,pageTitle string) (string,error) {
+func (shortLink *ShortLink) GenerateShortLinkTemp(PageURL, pageTitle string) (string, error) {
 	return shortLink.generate(ShortLinker{
-		PageUrl:     pageUrl,
+		PageURL:     PageURL,
 		PageTitle:   pageTitle,
 		IsPermanent: false,
 	})
