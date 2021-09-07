81eb9d20ef1c53dbc55cecd26257c6b9e23cfac8
houseme <qzg40737@163.com>
2021-09-08 07:50:12 +0800

    [feature] CheckEncryptedData (https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/user-info/auth.checkEncryptedData.html)

--- miniprogram/auth/auth.go
+++ miniprogram/auth/auth.go
@@ -10,6 +10,8 @@ import (
 
 const (
 	code2SessionURL = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
+
+	checkEncryptedDataURL = "https://api.weixin.qq.com/wxa/business/checkencryptedmsg?access_token=%s"
 )
 
 // Auth 登录/用户信息
@@ -31,6 +33,14 @@ type ResCode2Session struct {
 	UnionID    string `json:"unionid"`     // 用户在开放平台的唯一标识符，在满足UnionID下发条件的情况下会返回
 }
 
+// RspCheckEncryptedData .
+type RspCheckEncryptedData struct {
+	util.CommonError
+
+	Vaild      bool `json:"vaild"`       // 是否是合法的数据
+	CreateTime uint `json:"create_time"` // 加密数据生成的时间戳
+}
+
 // Code2Session 登录凭证校验。
 func (auth *Auth) Code2Session(jsCode string) (result ResCode2Session, err error) {
 	var response []byte
@@ -50,4 +60,26 @@ func (auth *Auth) Code2Session(jsCode string) (result ResCode2Session, err error
 // GetPaidUnionID 用户支付完成后，获取该用户的 UnionId，无需用户授权
 func (auth *Auth) GetPaidUnionID() {
 	// TODO
+}
+
+// CheckEncryptedData .检查加密信息是否由微信生成（当前只支持手机号加密数据），只能检测最近3天生成的加密数据
+func (auth *Auth) CheckEncryptedData(encryptedMsgHash string) (result RspCheckEncryptedData, err error) {
+	var response []byte
+	var (
+		at string
+	)
+	if at, err = auth.GetAccessToken(); err != nil {
+		return
+	}
+	if response, err = util.HTTPPost(fmt.Sprintf(checkEncryptedDataURL, at), "encrypted_msg_hash="+encryptedMsgHash); err != nil {
+		return
+	}
+	if err = json.Unmarshal(response, &result); err != nil {
+		return
+	}
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("CheckEncryptedData error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+	return
 }
