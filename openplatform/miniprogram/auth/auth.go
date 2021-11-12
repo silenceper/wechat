2fab35226e96bb0237c8d7990e4bfde6b38e09fd
zhifu.tang@papa.com.cn <a123456>
2021-11-12 10:32:05 +0800

    授权方appID

---
+++ openplatform/miniprogram/auth/auth.go
@@ -0,0 +1,95 @@
+package auth
+
+import (
+	context2 "context"
+	"encoding/json"
+	"fmt"
+	"github.com/silenceper/wechat/v2/openplatform/context"
+	"github.com/silenceper/wechat/v2/util"
+)
+
+
+const (
+	code2SessionURL = "https://api.weixin.qq.com/sns/component/jscode2session?appid=%s&js_code=%s&grant_type=authorization_code&component_appid=%s&component_access_token=%s"
+	checkEncryptedDataURL = "https://api.weixin.qq.com/wxa/business/checkencryptedmsg?access_token=%s"
+)
+
+// Auth 登录/用户信息
+type Auth struct {
+	*context.Context
+	appID string
+}
+
+// NewAuth new auth (授权方appID)
+func NewAuth(ctx *context.Context,appID string) *Auth {
+	return &Auth{ctx,appID}
+}
+
+// ResCode2Session 登录凭证校验的返回结果
+type ResCode2Session struct {
+	util.CommonError
+	OpenID     string `json:"openid"`      // 用户唯一标识
+	SessionKey string `json:"session_key"` // 会话密钥
+	UnionID    string `json:"unionid"`     // 用户在开放平台的唯一标识符，在满足UnionID下发条件的情况下会返回
+}
+
+// RspCheckEncryptedData .
+type RspCheckEncryptedData struct {
+	util.CommonError
+	Vaild      bool `json:"vaild"`       // 是否是合法的数据
+	CreateTime uint `json:"create_time"` // 加密数据生成的时间戳
+}
+
+// Code2Session 登录凭证校验。
+func (auth *Auth) Code2Session(jsCode string) (result ResCode2Session, err error) {
+	return auth.Code2SessionContext(context2.Background(), jsCode)
+}
+
+// Code2SessionContext 登录凭证校验。
+func (auth *Auth) Code2SessionContext(ctx context2.Context, jsCode string) (result ResCode2Session, err error) {
+	var response []byte
+	var componentAccessToken string
+	componentAccessToken,err=auth.GetComponentAccessToken()
+	if err!=nil{
+		return
+	}
+	if response, err = util.HTTPGetContext(ctx, fmt.Sprintf(code2SessionURL, auth.appID,auth.Context.AppID,  jsCode,componentAccessToken)); err != nil {
+		return
+	}
+	if err = json.Unmarshal(response, &result); err != nil {
+		return
+	}
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("Code2Session error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+	return
+}
+
+// GetPaidUnionID 用户支付完成后，获取该用户的 UnionId，无需用户授权
+func (auth *Auth) GetPaidUnionID() {
+	// TODO
+}
+
+// CheckEncryptedData .检查加密信息是否由微信生成（当前只支持手机号加密数据），只能检测最近3天生成的加密数据
+func (auth *Auth) CheckEncryptedData(encryptedMsgHash string) (result RspCheckEncryptedData, err error) {
+	return auth.CheckEncryptedDataContext(context2.Background(), encryptedMsgHash)
+}
+
+// CheckEncryptedDataContext .检查加密信息是否由微信生成（当前只支持手机号加密数据），只能检测最近3天生成的加密数据
+func (auth *Auth) CheckEncryptedDataContext(ctx context2.Context, encryptedMsgHash string) (result RspCheckEncryptedData, err error) {
+	var response []byte
+	var (
+		at string
+	)
+	if at, err = auth.GetAccessToken(); err != nil {
+		return
+	}
+	if response, err = util.HTTPPostContext(ctx, fmt.Sprintf(checkEncryptedDataURL, at), "encrypted_msg_hash="+encryptedMsgHash); err != nil {
+		return
+	}
+	if err = util.DecodeWithError(response, &result, "CheckEncryptedDataAuth"); err != nil {
+		return
+	}
+	return
+}
--- openplatform/miniprogram/miniprogram.go
+++ openplatform/miniprogram/miniprogram.go
@@ -1,6 +1,8 @@
 package miniprogram
 
 import (
+	"github.com/silenceper/wechat/v2/credential"
+	"github.com/silenceper/wechat/v2/openplatform/miniprogram/auth"
 	openContext "github.com/silenceper/wechat/v2/openplatform/context"
 	"github.com/silenceper/wechat/v2/openplatform/miniprogram/basic"
 	"github.com/silenceper/wechat/v2/openplatform/miniprogram/component"
@@ -29,4 +31,14 @@ func (miniProgram *MiniProgram) GetComponent() *component.Component {
 // GetBasic 基础信息设置
 func (miniProgram *MiniProgram) GetBasic() *basic.Basic {
 	return basic.NewBasic(miniProgram.openContext, miniProgram.AppID)
+}
+
+// GetAuth 登录/用户信息相关接口
+func (miniProgram *MiniProgram) GetAuth() *auth.Auth {
+	return auth.NewAuth(miniProgram.openContext, miniProgram.AppID)
+}
+
+// SetAccessTokenHandle 自定义access_token获取方式
+func (miniProgram *MiniProgram) SetAccessTokenHandle(accessTokenHandle credential.AccessTokenHandle) {
+	miniProgram.openContext.AccessTokenHandle = accessTokenHandle
 }
