9cecda04697f0e92602d9cf9efb9b4afd3d47b25
意琦行 <31980412+lixd@users.noreply.github.com>
2021-12-07 19:05:37 +0800

    小程序码、URL Link 增加参数(#502,#512) (#514)
    * 小程序码、URL Link 增加参数(#502,#512)
    
    * gofmt

--- miniprogram/qrcode/qrcode.go
+++ miniprogram/qrcode/qrcode.go
@@ -40,6 +40,8 @@ type QRCoder struct {
 	Page string `json:"page,omitempty"`
 	// path 扫码进入的小程序页面路径
 	Path string `json:"path,omitempty"`
+	// checkPath 检查page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但page 有数量上限（60000个）请勿滥用
+	CheckPath bool `json:"check_path,omitempty"`
 	// width 图片宽度
 	Width int `json:"width,omitempty"`
 	// scene 最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
@@ -50,6 +52,8 @@ type QRCoder struct {
 	LineColor Color `json:"line_color,omitempty"`
 	// isHyaline 是否需要透明底色
 	IsHyaline bool `json:"is_hyaline,omitempty"`
+	// envVersion 要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"
+	EnvVersion string `json:"env_version,omitempty"`
 }
 
 // fetchCode 请求并返回二维码二进制数据
