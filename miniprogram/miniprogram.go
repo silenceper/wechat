56350c3655c9dc22e045388ac43adf67256d79b1
save95 <3973008+save95@users.noreply.github.com>
2022-04-22 10:09:27 +0800

    add: [小程序] 增加 安全风控、内容安全1.0 & 2.0、code换取手机号 (#554)
    * add: [小程序] 增加 安全风控、内容安全1.0 & 2.0、code换取手机号
    
    * add: check suggest docs
    
    * fix
    
    * fix
    
    Co-authored-by: luoyu <luoyu@medlinker.com>

--- miniprogram/miniprogram.go
+++ miniprogram/miniprogram.go
@@ -4,6 +4,7 @@ import (
 	"github.com/silenceper/wechat/v2/credential"
 	"github.com/silenceper/wechat/v2/miniprogram/analysis"
 	"github.com/silenceper/wechat/v2/miniprogram/auth"
+	"github.com/silenceper/wechat/v2/miniprogram/business"
 	"github.com/silenceper/wechat/v2/miniprogram/config"
 	"github.com/silenceper/wechat/v2/miniprogram/content"
 	"github.com/silenceper/wechat/v2/miniprogram/context"
@@ -11,6 +12,8 @@ import (
 	"github.com/silenceper/wechat/v2/miniprogram/message"
 	"github.com/silenceper/wechat/v2/miniprogram/privacy"
 	"github.com/silenceper/wechat/v2/miniprogram/qrcode"
+	"github.com/silenceper/wechat/v2/miniprogram/riskcontrol"
+	"github.com/silenceper/wechat/v2/miniprogram/security"
 	"github.com/silenceper/wechat/v2/miniprogram/shortlink"
 	"github.com/silenceper/wechat/v2/miniprogram/subscribe"
 	"github.com/silenceper/wechat/v2/miniprogram/tcb"
@@ -59,6 +62,11 @@ func (miniProgram *MiniProgram) GetAnalysis() *analysis.Analysis {
 	return analysis.NewAnalysis(miniProgram.ctx)
 }
 
+// GetBusiness 业务接口
+func (miniProgram *MiniProgram) GetBusiness() *business.Business {
+	return business.NewBusiness(miniProgram.ctx)
+}
+
 // GetPrivacy 小程序隐私协议相关API
 func (miniProgram *MiniProgram) GetPrivacy() *privacy.Privacy {
 	return privacy.NewPrivacy(miniProgram.ctx)
@@ -97,6 +105,16 @@ func (miniProgram *MiniProgram) GetContentSecurity() *content.Content {
 // GetURLLink 小程序URL Link接口
 func (miniProgram *MiniProgram) GetURLLink() *urllink.URLLink {
 	return urllink.NewURLLink(miniProgram.ctx)
+}
+
+// GetRiskControl 安全风控接口
+func (miniProgram *MiniProgram) GetRiskControl() *riskcontrol.RiskControl {
+	return riskcontrol.NewRiskControl(miniProgram.ctx)
+}
+
+// GetSecurity 内容安全接口
+func (miniProgram *MiniProgram) GetSecurity() *security.Security {
+	return security.NewSecurity(miniProgram.ctx)
 }
 
 // GetShortLink 小程序短链接口
