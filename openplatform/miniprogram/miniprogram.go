ccc8018df974ee954c8aa06e4b454737463fc19f
theodore <zhouzhe5934@163.COM>
2022-06-28 02:28:58 +0800

    小程序服务商 - 获取 URL Link 及 查询小程序 url_link 配置

--- openplatform/miniprogram/miniprogram.go
+++ openplatform/miniprogram/miniprogram.go
@@ -1,6 +1,9 @@
 package miniprogram
 
 import (
+	"fmt"
+	context2 "github.com/silenceper/wechat/v2/miniprogram/context"
+	"github.com/silenceper/wechat/v2/miniprogram/urllink"
 	openContext "github.com/silenceper/wechat/v2/openplatform/context"
 	"github.com/silenceper/wechat/v2/openplatform/miniprogram/basic"
 	"github.com/silenceper/wechat/v2/openplatform/miniprogram/component"
@@ -10,6 +13,30 @@ import (
 type MiniProgram struct {
 	AppID       string
 	openContext *openContext.Context
+
+	authorizerRefreshToken string
+}
+
+// GetAccessToken 获取ak
+func (miniProgram *MiniProgram) GetAccessToken() (string, error) {
+	ak, akErr := miniProgram.openContext.GetAuthrAccessToken(miniProgram.AppID)
+	if akErr == nil {
+		return ak, nil
+	}
+	if miniProgram.authorizerRefreshToken == "" {
+		return "", fmt.Errorf("please set the authorizer_refresh_token first")
+	}
+	akRes, akResErr := miniProgram.GetComponent().RefreshAuthrToken(miniProgram.AppID, miniProgram.authorizerRefreshToken)
+	if akResErr != nil {
+		return "", akResErr
+	}
+	return akRes.AccessToken, nil
+}
+
+// SetAuthorizerRefreshToken 设置代执操作业务授权账号authorizer_refresh_token
+func (miniProgram *MiniProgram) SetAuthorizerRefreshToken(authorizerRefreshToken string) *MiniProgram {
+	miniProgram.authorizerRefreshToken = authorizerRefreshToken
+	return miniProgram
 }
 
 // NewMiniProgram 实例化
@@ -29,4 +56,14 @@ func (miniProgram *MiniProgram) GetComponent() *component.Component {
 // GetBasic 基础信息设置
 func (miniProgram *MiniProgram) GetBasic() *basic.Basic {
 	return basic.NewBasic(miniProgram.openContext, miniProgram.AppID)
+}
+
+/*
+GetURLLink 小程序URL Link接口
+调用前需确认已调用 SetAuthorizerRefreshToken 避免由于缓存中 authorizer_access_token 过期执行中断
+*/
+func (miniProgram *MiniProgram) GetURLLink() *urllink.URLLink {
+	return urllink.NewURLLink(&context2.Context{
+		AccessTokenHandle: miniProgram,
+	})
 }
