4ca59ec9b7fb8b152bbe0bfbba6b04746917d341
wang.yu <wangyu@uniondrug.com>
2022-08-07 14:53:46 +0800

    获取用户信息

--- work/suite/oauth.go
+++ work/suite/oauth.go
@@ -1,6 +1,7 @@
 package suite
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/silenceper/wechat/v2/util"
@@ -9,6 +10,8 @@ import (
 const (
 	// GetUserinfo3rdURL 获取访问用户身份
 	GetUserinfo3rdURL = "https://qyapi.weixin.qq.com/cgi-bin/service/getuserinfo3rd?suite_access_token=%s&code=%s"
+	// GetUserDetail3rdURL 获取访问用户敏感信息
+	GetUserDetail3rdURL = "https://qyapi.weixin.qq.com/cgi-bin/service/getuserdetail3rd?suite_access_token=%s"
 )
 
 type (
@@ -44,6 +47,39 @@ func (r *Client) GetUserinfo3rd(code string) (*GetUserinfo3rdResponse, error) {
 	return result, nil
 }
 
-func (r *Client) TestFunc() string {
-	return r.SuiteAccessToken + "success"
+type (
+	// GetUserDetail3rdRequest 获取访问用户敏感信息请求
+	GetUserDetail3rdRequest struct {
+		UserTicket string `json:"user_ticket"`
+	}
+	// GetUserDetail3rdResponse 获取访问用户敏感信息响应
+	GetUserDetail3rdResponse struct {
+		util.CommonError
+		CorpID string `json:"corpid"`
+		UserID string `json:"userid"`
+		Name   string `json:"name"`
+		Gender string `json:"gender"`
+		Avatar string `json:"avatar"`
+		QrCode string `json:"qr_code"`
+	}
+)
+
+// GetUserDetail3rd 获取访问用户敏感信息
+// see https://developer.work.weixin.qq.com/document/path/91122
+func (r *Client) GetUserDetail3rd(request *GetUserDetail3rdRequest) (*GetUserDetail3rdResponse, error) {
+	var (
+		response []byte
+		err      error
+	)
+	jsonData, _ := json.Marshal(request)
+	response, err = util.HTTPPost(fmt.Sprintf(GetUserDetail3rdURL, r.SuiteAccessToken), string(jsonData))
+	if err != nil {
+		return nil, err
+	}
+	var result *GetUserDetail3rdResponse
+	err = util.DecodeWithError(response, &result, "GetUserDetail3rd")
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
