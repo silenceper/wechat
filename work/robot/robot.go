f6cc676c29dcebc2f154b19bf8352ab5458a1159
cxc <chenprg@gmail.com>
2022-09-21 12:20:52 +0800

    fix: 删除注释多余字符

--- work/robot/robot.go
+++ work/robot/robot.go
@@ -12,7 +12,7 @@ const (
 	WebhookSendURL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s"
 )
 
-// RobotBroadcast 群机器人消息发送s
+// RobotBroadcast 群机器人消息发送
 // @see https://developer.work.weixin.qq.com/document/path/91770
 func (r *Client) RobotBroadcast(webhookKey string, options interface{}) (info util.CommonError, err error) {
 	var data []byte
