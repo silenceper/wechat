5380d5bee7a43cb89a9d88fa22767245e8dd4875
okhowang <3352585+okhowang@users.noreply.github.com>
2022-09-29 14:33:53 +0800

    GetAccessToken支持Context (#618)

--- officialaccount/officialaccount.go
+++ officialaccount/officialaccount.go
@@ -1,6 +1,7 @@
 package officialaccount
 
 import (
+	stdcontext "context"
 	"net/http"
 
 	"github.com/silenceper/wechat/v2/officialaccount/draft"
@@ -91,6 +92,14 @@ func (officialAccount *OfficialAccount) GetServer(req *http.Request, writer http
 
 // GetAccessToken 获取access_token
 func (officialAccount *OfficialAccount) GetAccessToken() (string, error) {
+	return officialAccount.ctx.GetAccessToken()
+}
+
+// GetAccessTokenContext 获取access_token
+func (officialAccount *OfficialAccount) GetAccessTokenContext(ctx stdcontext.Context) (string, error) {
+	if c, ok := officialAccount.ctx.AccessTokenHandle.(credential.AccessTokenContextHandle); ok {
+		return c.GetAccessTokenContext(ctx)
+	}
 	return officialAccount.ctx.GetAccessToken()
 }
 
