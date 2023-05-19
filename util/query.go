d0cd099fa1d3edd3d21514b30ec50b36c48ea3c7
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-19 20:25:28 +0800

    fix: fixed type convert error

--- util/query.go
+++ util/query.go
@@ -1,15 +1,12 @@
 package util
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 // Query 将Map序列化为Query参数
-func Query(params map[string]interface{}) string {
+func Query(params map[string]string) string {
 	finalString := make([]string, 0)
 	for key, value := range params {
-		finalString = append(finalString, strings.Join([]string{key, fmt.Sprintf("%s", value)}, "="))
+		finalString = append(finalString, strings.Join([]string{key, value}, "="))
 	}
 	return strings.Join(finalString, "&")
 }
