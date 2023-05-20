9cee2abb58da873b970f432f679e68aa626e624f
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-20 18:03:32 +0800

    ✨ feat: template string support any type

--- util/template_test.go
+++ util/template_test.go
@@ -6,13 +6,15 @@ import (
 
 // TestTemplate testing case about Template method
 func TestTemplate(t *testing.T) {
-	result := Template("{name}={age};{with}={another}", map[string]string{
+	result := Template("{name}={age};{with}={another};any={any};boolean={boolean}", map[string]interface{}{
 		"name":    "Helan",
 		"age":     "33",
 		"with":    "Pep",
 		"another": "C",
+		"any":     33,
+		"boolean": false,
 	})
-	if result != "Helan=33;Pep=C" {
+	if result != "Helan=33;Pep=C;any=33;boolean=false" {
 		t.Error("NOT PSS testing")
 	}
 }
