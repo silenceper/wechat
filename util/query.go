14db54b696150819b119994db63cc81e8abb9935
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-20 17:44:19 +0800

    ✨ feat: support for unsigned integers

--- util/query.go
+++ util/query.go
@@ -11,7 +11,7 @@ func Query(params map[string]interface{}) string {
 	for key, value := range params {
 		valueString := ""
 		switch v := value.(type) {
-		case int, int8, int16, int32, int64:
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
 			valueString = fmt.Sprintf("%d", v)
 		case bool:
 			valueString = fmt.Sprintf("%v", v)
