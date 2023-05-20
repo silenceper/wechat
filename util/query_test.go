1798c4748212e0a6463fe7c18184a396fb3b374c
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-05-20 13:51:27 +0800

    del: remove useless code

--- util/query_test.go
+++ util/query_test.go
@@ -13,7 +13,6 @@ func TestQuery(t *testing.T) {
 		"cat":  "Peter",
 	})
 	if result != "age=12&name=Alan&cat=Peter" || result == "cat=Peter&age=12&name=Alan" {
-		fmt.Printf("%v", result)
 		t.Error("NOT PASS")
 	}
 }
