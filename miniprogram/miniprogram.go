e02af1dc0ff5c635b9f94ea5b8e9dfc222e2a2df
ccfish <ccfish@ccfish.net>
2023-08-27 11:25:05 +0800

    小程序发货信息管理 (#710)
    * 小程序发货信息管理
    
    * fix golang lint
    
    * fix miss
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * 修复查询参数last_index=“”时异常
    
    * Update miniprogram.go

--- miniprogram/miniprogram.go
+++ miniprogram/miniprogram.go
@@ -12,6 +12,7 @@ import (
 	"github.com/silenceper/wechat/v2/miniprogram/encryptor"
 	"github.com/silenceper/wechat/v2/miniprogram/message"
 	"github.com/silenceper/wechat/v2/miniprogram/minidrama"
+	"github.com/silenceper/wechat/v2/miniprogram/order"
 	"github.com/silenceper/wechat/v2/miniprogram/privacy"
 	"github.com/silenceper/wechat/v2/miniprogram/qrcode"
 	"github.com/silenceper/wechat/v2/miniprogram/riskcontrol"
@@ -138,6 +139,11 @@ func (miniProgram *MiniProgram) GetOpenAPI() *openapi.OpenAPI {
 // GetVirtualPayment 小程序虚拟支付
 func (miniProgram *MiniProgram) GetVirtualPayment() *virtualpayment.VirtualPayment {
 	return virtualpayment.NewVirtualPayment(miniProgram.ctx)
+}
+
+// GetShipping 小程序发货信息管理服务
+func (miniProgram *MiniProgram) GetShipping() *order.Shipping {
+	return order.NewShipping(miniProgram.ctx)
 }
 
 // GetMiniDrama 小程序娱乐微短剧
