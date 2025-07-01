9f1c48e6ef69605152415e764d686022e85bd762
markwang <www.wang61@qq.com>
2025-07-01 15:44:53 +0800

    feat: 微信小程序-运维中心

--- miniprogram/miniprogram.go
+++ miniprogram/miniprogram.go
@@ -12,6 +12,7 @@ import (
 	"github.com/silenceper/wechat/v2/miniprogram/encryptor"
 	"github.com/silenceper/wechat/v2/miniprogram/message"
 	"github.com/silenceper/wechat/v2/miniprogram/minidrama"
+	"github.com/silenceper/wechat/v2/miniprogram/operation"
 	"github.com/silenceper/wechat/v2/miniprogram/order"
 	"github.com/silenceper/wechat/v2/miniprogram/privacy"
 	"github.com/silenceper/wechat/v2/miniprogram/qrcode"
@@ -178,4 +179,9 @@ func (miniProgram *MiniProgram) GetRedPacketCover() *redpacketcover.RedPacketCov
 // GetUpdatableMessage 小程序动态消息
 func (miniProgram *MiniProgram) GetUpdatableMessage() *message.UpdatableMessage {
 	return message.NewUpdatableMessage(miniProgram.ctx)
+}
+
+// GetOperation 小程序运维中心
+func (miniProgram *MiniProgram) GetOperation() *operation.Operation {
+	return operation.NewOperation(miniProgram.ctx)
 }
